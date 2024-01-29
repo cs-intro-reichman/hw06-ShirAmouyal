b9ae8045e22363f5b53561581742c6d540789864
Shir Amouyal <154405205+ShirAmouyal@users.noreply.github.com>
2024-01-29 22:44:08 +0200

    with changes

--- Runigram.java
+++ Runigram.java
@@ -109,7 +109,7 @@ public class Runigram {
 		for(int i=0;i<rows;i++){
 			for(int k=0;k<cols;k++){
 				
-			 Nimage[i][k]=image[(rows-1)-i][k];
+			 Nimage[i][k]=image[rows-1-i][k];
 			}
 		}
 		return Nimage;
@@ -178,6 +178,9 @@ public class Runigram {
 	 */
 	public static Color[][] blend(Color[][] image1, Color[][] image2, double alpha) {
 		Color[][] belndColor=new Color[image1.length][image1[0].length];
+		if(image1.length != image2.length || image1[0].length!=image2[0].length){
+			image2=scaled(image2,image1[0].length, image1.length)
+		}
 		for(int i=0;i<image1.length;i++){
 			for(int j=0;j<image1[0].length;j++){
 				belndColor[i][j]=blend(image1[i][j],image2[i][j], alpha);
